52021b7b6e6bef3fda374e1b05a47f3a16027e0c
Fant1covskiy <almeraerov@gmail.com>
2026-02-01 22:03:08 +0500

    14156

--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -73,6 +73,7 @@ export class PaymentsService {
         orderId: order.id,
         qrId,
         qrPayload,
+        payUrl: qrPayload,
         imageBase64: data?.Data?.image?.content ?? null,
       };
     } catch (e: any) {
