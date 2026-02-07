d1ba08d36dcc647a455bfc001d77119e487fdf49
Fant1covskiy <almeraerov@gmail.com>
2026-02-08 00:37:07 +0500

    zhanymsabi

--- src/modules/styles/styles.module.ts
+++ src/modules/styles/styles.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StylesController } from './styles.controller';
 import { StylesService } from './styles.service';
-import { Style } from './entities/style.entity';
+import { Style } from './style.entity';
+import { Category } from '../categories/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Style])],
+  imports: [TypeOrmModule.forFeature([Style, Category])],
   controllers: [StylesController],
   providers: [StylesService],
   exports: [StylesService],
